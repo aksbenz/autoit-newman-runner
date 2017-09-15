a2eaa4f47dcf349095393ccd421addec82a575f9
Akash <akashbainslay@gmail.com>
2017-09-15 16:31:21 +1000

    handle duplicate folder names

--- jsonreader.js
+++ jsonreader.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// We Use node enclose to create an exe to be used by autoit
 fs.writeFileSync("args.dat", process.argv.join(','));
 
 if (process.argv.length < 4) {
@@ -17,21 +18,33 @@ var paths = [],
     stack = [],
     base = c.item;
 
+var idx = base.length;
 // Push to stack all 1st level folders in reverse order
-base.reverse().forEach(function(i) { stack.push(i) });
+base.reverse().forEach(function(i) {
+    i.name = 'O' + idx-- + '_' + i.name;
+    stack.push(i)
+});
+
+idx++;
 var parent;
 while (stack.length > 0) {
     var fld = stack.pop();
     if (fld.item) {
+        console.log(fld.name);
         paths.push(getParentTree(fld));
-        fld.item.reverse().forEach(function(i) { i.parent = fld; if (i.item) stack.push(i); });
+        fld.item.reverse().forEach(function(i) {
+            i.parent = fld;
+            if (i.item) {
+                i.name = 'O' + idx++ + '_' + i.name;
+                stack.push(i);
+            }
+        });
     }
 }
 
 console.log('*****************');
 console.log(paths.join('\n'));
 fs.writeFileSync(tgtFile, paths.join('\r\n'));
-
 
 function getParentTree(fld) {
     var path = fld.name.replace('/', '0x2f');
